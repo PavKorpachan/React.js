90c8b9e37c0071bf7375878f1f691152fe907238
PavKorpachan <pkorpachan@gmail.com>
2024-01-19 17:45:40 +0300

    glav

---
+++ Mysite/my-app/src/App.js
@@ -0,0 +1,13 @@
+import Glav from './glav/glav';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Glav />
+    </div>
+  );
+}
+
+export default App;
---
+++ Mysite/my-app/src/glav/glav.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import './glav.css'
+import bg from './bg1.png'
+import down from './download.svg'
+import google from './google.svg'
+import apple from './apple.svg'
+
+const glav = () => {
+  return (
+    <div className='glav'>
+        <div className='nav'>
+            <p className='logo'>
+                PocketPal
+            </p>
+            <div className='nav_mini'>
+                <p className='nav_mini_text'>
+                    Home
+                </p>
+                <p className='nav_mini_text'>
+                    About
+                </p>
+                <p className='nav_mini_text'>
+                    Testimonials
+                </p>
+                <p className='nav_mini_text'>
+                    Contact
+                </p>
+            </div>
+            <img src={down} alt='' />
+        </div>
+        <div className='glav_text_flex'>
+            <div className='glav_text'>
+                <p>
+                    Take  control of your finances with our <span>Budgeting</span> tool
+                </p>
+                <p className='glav_text_mini'>
+                    Set a budget, track your spending, and reach your financial goals with our easy-to-use budgeting app. Get alerts as you approach your budget limits and stsy on track to financial freedom. Download the app now and take control of your finances
+                </p>
+                <div className='glav_text_svg'>
+                    <img src={apple} alt='' />
+                    <img src={google} alt='' />
+                </div>
+            </div>
+        </div>
+        <img className='bg' src={bg} alt='' />
+    </div>
+  )
+}
+
+export default glav
